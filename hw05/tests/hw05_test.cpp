4f5861fad850e728a3cb6b5332a4a4bb1da733a1
DanielGim-Hub <hamdan65373@gmail.com>
2024-12-11 06:05:34 +0300

    add hw05

--- hw05/tests/hw05_test.cpp
+++ hw05/tests/hw05_test.cpp
@@ -41,47 +41,6 @@ TEST(Hw05Tests, Test5)
     EXPECT_EQ(actual, expected);
 }
 
-// Дополнительные тесты
-TEST(Hw05Tests, Test6)
-{
-    VecOfStr input{"", "a", "aa", "abcba", "not a palindrome"};
-    auto expected = VecOfStr{"", "a", "aa", "abcba"};
-    auto actual = hw05(input);
-    EXPECT_EQ(actual, expected);
-}
-
-TEST(Hw05Tests, Test7)
-{
-    VecOfStr input{"Was it a car or a cat I saw?", "hello world"};
-    auto expected = VecOfStr{"Was it a car or a cat I saw?"};
-    auto actual = hw05(input);
-    EXPECT_EQ(actual, expected);
-}
-
-TEST(Hw05Tests, Test8)
-{
-    VecOfStr input{"No lemon, no melon", "123321", "random"};
-    auto expected = VecOfStr{"No lemon, no melon", "123321"};
-    auto actual = hw05(input);
-    EXPECT_EQ(actual, expected);
-}
-
-TEST(Hw05Tests, Test9)
-{
-    VecOfStr input{"Able was I, I saw Elba", "Palindrome"};
-    auto expected = VecOfStr{"Able was I, I saw Elba"};
-    auto actual = hw05(input);
-    EXPECT_EQ(actual, expected);
-}
-
-TEST(Hw05Tests, Test10)
-{
-    VecOfStr input{"Eva, can I see bees in a cave?"};
-    auto expected = VecOfStr{"Eva, can I see bees in a cave?"};
-    auto actual = hw05(input);
-    EXPECT_EQ(actual, expected);
-}
-
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
